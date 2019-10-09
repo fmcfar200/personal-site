f4692800ce020b5475d676ac751488870898badd
fmcfar200 <fmcfar200@caledonian.ac.uk>
2019-10-09 23:09:11 +0100

    starter

---
+++ src/components/aboutMe.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../styles/aboutMe.css";
+
+const AboutMe = () => {
+  return (
+    <div className="about-container">
+      <img className="headshot-image" src="headshot.jpg" alt="headshot"></img>
+      <div className="info-container">
+        <div className="about-header">
+          <h3>About Me</h3>
+          <div className="line"></div>
+        </div>
+        <p>
+          hello my name is fraser, this is placeholder text for my personal
+          website. So please ignore cause theres really no point reading this
+          because its pretty much a load of gibberish.
+        </p>
+        <p>
+          hello my name is fraser, this is placeholder text for my personal
+          website. So please ignore cause theres really no point reading this
+          because its pretty much a load of gibberish.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutMe;
