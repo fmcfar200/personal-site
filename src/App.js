15d9ba95c0dfb15f5a461823440cb7925b86a672
fmcfar200 <fmcfar200@caledonian.ac.uk>
2019-11-13 22:55:27 +0000

    header refactor + minor fixes

--- src/App.js
+++ src/App.js
@@ -4,27 +4,15 @@ import MyProjects from "./components/myProjects";
 import OtherProjects from "./components/otherProjects";
 import NavBar from "./components/navbar";
 import MobileNavbar from "./components/mobileNavbar";
-import "./App.css";
-import Heading from "./components/heading";
 import ContactMe from "./components/contactMe";
-import ContactButton from "./components/contactButton";
+import "./App.css";
+import Header from "./components/header";
 
 function App() {
   return (
     <React.Fragment>
       <MobileNavbar />
-      <div className="header">
-        <p className="header-text-1">Hello, I'm</p>
-        <p className="header-text-2">Fraser McFarlane.</p>
-        <p className="header-text-3">
-          I create software for the web, mobile devices and sometimes games
-        </p>
-        <p className="header-text-4" style={{ marginBottom: "30px" }}>
-          I am a Software Developer based in Glasgow, Scotland currently focused
-          on developing mobile/web applications.
-        </p>
-        <ContactButton />
-      </div>
+      <Header />
       <NavBar />
       <main className="App">
         <AboutMe />
---
+++ src/components/header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ContactButton from "./contactButton";
+import "../styles/header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <p className="header-text-1">Hello, I'm</p>
+      <p className="header-text-2">Fraser McFarlane.</p>
+      <p className="header-text-3">
+        I create software for the web, mobile devices and sometimes games
+      </p>
+      <p className="header-text-4" style={{ marginBottom: "30px" }}>
+        I am a Software Developer based in Glasgow, Scotland currently focused
+        on developing mobile/web applications.
+      </p>
+      <ContactButton />
+    </div>
+  );
+};
+
+export default Header;
