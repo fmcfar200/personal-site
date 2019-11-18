68249ba435e30299a4dec653f0085ff916b54a05
fmcfar200 <fmcfar200@caledonian.ac.uk>
2019-11-18 15:23:58 +0000

    added content

--- src/components/aboutMe.jsx
+++ src/components/aboutMe.jsx
@@ -10,15 +10,27 @@ const AboutMe = () => {
       <div className="info-container">
         <Heading number="01" title="About Me" />
         <p>
-          hello my name is fraser, this is placeholder text for my personal
-          website. So please ignore cause theres really no point reading this
-          because its pretty much a load of gibberish.
+          Hello, I'm Fraser. I am a software development graduate based in
+          Glasgow, Scotland who is passionate about building technological
+          solutions for the internet, mobile apps and in the past computer
+          games.
         </p>
         <p>
-          hello my name is fraser, this is placeholder text for my personal
-          website. So please ignore cause theres really no point reading this
-          because its pretty much a load of gibberish.
+          Last year, i graduated from <span>Glasgow Caledonian University</span>{" "}
+          with a <span>1st class</span> honours degree in Software Development.
+          Since then i have been refining the programming and development skills
+          by working on personal projects and some freelance work for small
+          businesses.
         </p>
+        <p>Here are some technologies i have been working with a lot:</p>
+        <ul className="tech-list">
+          <li className="tech-item">Java</li>
+          <li className="tech-item">Android Studio</li>
+          <li className="tech-item"> JavaScript (ES6)</li>
+          <li className="tech-item">HTML\CSS</li>
+          <li className="tech-item">React.js</li>
+          <li className="tech-item">Xamarin</li>
+        </ul>
       </div>
     </div>
   );
