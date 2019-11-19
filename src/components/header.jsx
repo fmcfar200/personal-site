9b05a6aef4c7798e5564c9e88a8528b163761593
fmcfar200 <fmcfar200@caledonian.ac.uk>
2019-11-19 00:22:20 +0000

    animations to header and nav

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,16 +5,18 @@ import "../styles/header.css";
 const Header = () => {
   return (
     <div className="header">
-      <p className="header-text-1">Hello, I'm</p>
-      <p className="header-text-2">Fraser McFarlane.</p>
-      <p className="header-text-3">
-        I create software for the web, mobile devices and sometimes games
-      </p>
-      <p className="header-text-4" style={{ marginBottom: "30px" }}>
-        I am a Software Developer based in Glasgow, Scotland currently focused
-        on developing mobile/web applications.
-      </p>
-      <ContactButton />
+      <div>
+        <p className="header-text-1">Hello, I'm</p>
+        <p className="header-text-2">Fraser McFarlane.</p>
+        <p className="header-text-3">
+          I create software for the web, mobile devices and sometimes games
+        </p>
+        <p className="header-text-4" style={{ marginBottom: "30px" }}>
+          I am a Software Developer based in Glasgow, Scotland currently focused
+          on developing mobile/web applications.
+        </p>
+        <ContactButton />
+      </div>
     </div>
   );
 };
